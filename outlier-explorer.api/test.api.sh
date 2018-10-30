d69f52a8e1744c49a253cde5e9adbe6e28c11262
Robin Lamarche-Perrin <Robin.Lamarche-Perrin@mis.mpg.de>
2018-10-30 16:29:04 +0100

    Testing Outlier Explorer API

--- outlier-explorer.api/test.api.sh
+++ outlier-explorer.api/test.api.sh
@@ -22,14 +22,25 @@ curl -s http://localhost:8000/tools/outliers -d '{"dataset":"guardian.2016","par
 
 
 ## Test full pipeline (ON SERVER)
+## Check connexion with the Web service
 curl -s https://penelope.huma-num.fr/tools/ping
 
-curl -H "Content-Type: application/json" -s https://www.fcg-net.org/penelope/data/comment_structure -d '{"collection":"GuardianArticles", "start_date":"2017-01-01T00:00:00.000Z", "end_date":"2017-06-01T00:00:00.000Z"}' > guardian.raw
+## Find outliers in a preliminary stored Guardian dataset
+curl -s https://penelope.huma-num.fr/tools/outliers -d '{"dataset": "guardian.2016", "param": {"select": [{"dim": "topic", "select": "some", "head": 5}, {"dim": "week", "select": "all"}], "normalise": ["topic", "week"], "stat.test": {"type": "poisson", "threshold": 1}}}'
 
-echo '{"data":'$(< guardian.raw)',"param":{"time":{"reference":"comment","resolution":"day"}}}' | curl -s https://penelope.huma-num.fr/tools/format_comments -d @- > guardian.cube
+## Get Guardian data from www.fcg-net.org
+curl -H "Content-Type: application/json" -s https://www.fcg-net.org/penelope/data/comment_structure -d '{"collection": "GuardianArticles", "start_date": "2017-01-01T00:00:00.000Z", "end_date": "2017-06-01T00:00:00.000Z"}' > guardian.raw
 
-echo '{"data":'$(< guardian.cube)',"param":{"select":[{"dim":"time","select":"all"},{"dim":"topic","select":"some","head":5},{"dim":"user","select":"some","head":30}],"normalise":["topic","time"],"stat.test":{"type":"poisson","threshold":1}}}' | curl -s https://penelope.huma-num.fr/tools/outliers -d @- > guardian.outliers
+## Reformat Guardian data
+echo '{"data": '$(< guardian.raw)', "param": {"time": {"reference": "comment", "resolution": "day"}}}' | curl -s https://penelope.huma-num.fr/tools/format_comments -d @- > guardian.cube
 
-curl -s https://penelope.huma-num.fr/tools/outliers -d '{"dataset":"guardian.small","param":{"select":[{"dim":"topic","select":"some","head":5},{"dim":"week","select":"all"}],"normalise":["topic","week"],"stat.test":{"type":"poisson","threshold":0.5}}}'
+## Feed reformatted Guardian data to the Web Service
+echo '{"data": '$(< guardian.cube)', "param": {"select": [{"dim": "time", "select": "all"}, {"dim": "topic", "select": "some", "head": 5}, {"dim": "user", "select": "some", "head": 30}], "normalise": ["topic", "time"], "stat.test": {"type": "poisson", "threshold": 1}}}' | curl -s https://penelope.huma-num.fr/tools/outliers -d @- > guardian.outliers
 
-curl -s https://penelope.huma-num.fr/tools/outliers -d '{"dataset":"guardian.2016","param":{"select":[{"dim":"topic","select":"some","head":5},{"dim":"week","select":"all"}],"normalise":["topic","week"],"stat.test":{"type":"poisson","threshold":1}}}'
+
+
+## Request shape from SwaggerHub
+curl -X POST "http://localhost:8000/tools/outliers" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{  \"data\": [    {      \"user\": \"168821\",      \"topic\": \"keyword1\",      \"time\": \"2018-02-23\",      \"obs\": 0.5    },    {      \"user\": \"168821\",      \"topic\": \"keyword2\",      \"time\": \"2018-02-23\",      \"obs\": 0.5    },    {      \"user\": \"985003\",      \"topic\": \"keyword1\",      \"time\": \"2018-02-22\",      \"obs\": 0.5    },    {      \"user\": \"985003\",      \"topic\": \"keyword2\",      \"time\": \"2018-02-22\",      \"obs\": 0.5    }  ],  \"dataset\": null,  \"param\": {    \"select\": [      {        \"dim\": \"user\",        \"select\": \"some\",        \"list\": [          \"985002\"        ]      },      {        \"dim\": \"topic\",        \"select\": \"some\",        \"head\": 5      },      {        \"dim\": \"time\",        \"select\": \"all\"      }    ],    \"normalise\": [      \"topic\",      \"time\"    ],    \"stat.test\": {      \"type\": \"poisson\",      \"threshold\": 3    }  }}"
+
+
+curl -X POST "http://localhost:8000/tools/outliers" -H "accept: application/json" -H "Content-Type: application/json" -d '{ "data": [ { "user": "168821", "topic": "keyword1", "time": "2018-02-23", "obs": 0.5 }, { "user": "168821", "topic": "keyword2", "time": "2018-02-23", "obs": 0.5 }, { "user": "985003", "topic": "keyword1", "time": "2018-02-22", "obs": 0.5 }, { "user": "985003", "topic": "keyword2", "time": "2018-02-22", "obs": 0.5 } ], "dataset": null, "param": { "select": [ { "dim": "user", "select": "some", "list": [ "985003" ] }, { "dim": "topic", "select": "some", "head": 5 }, { "dim": "time", "select": "all" } ], "normalise": [ "topic", "time" ], "stat.test": { "type": "poisson", "threshold": 3 } }}'
