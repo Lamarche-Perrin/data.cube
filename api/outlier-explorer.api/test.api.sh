3c99cf91bb78b32f6515f5f538ffe329aab1bed2
Robin Lamarche-Perrin <Robin.Lamarche-Perrin@mis.mpg.de>
2019-06-26 16:34:46 +0200

    Writing geomedia tutorial. Part about normalisation models and outlier detection.

--- api/outlier-explorer.api/test.api.sh
+++ api/outlier-explorer.api/test.api.sh
@@ -1,3 +1,5 @@
+## LOCAL TESTS
+
 ## Test format_comments
 curl -s http://localhost:8000/format_comments -d '{"data":[{"article_tags":["keyword1","keyword2"],"data_published":"2018-02-22 16:48:23","comments":[{"author_id":"user1","time_stamp":"2018-02-23 10:45:01"},{"author_id":"user2","time_stamp":"2018-02-22 22:25:58"}]},{"article_tags":["keyword1","keyword2","keyword3"],"data_published":"2018-02-22 16:48:23","comments":[{"author_id":"user1","time_stamp":"2018-02-23 10:45:01"},{"author_id":"user2","time_stamp":"2018-02-22 22:25:58"},{"author_id":"user2","time_stamp":"2018-02-23 22:25:58"}]}],"param":{"time":{"reference":"comment","resolution":"day"}}}'
 
@@ -7,10 +9,22 @@ curl -s http://localhost:8000/outliers -d '{"data":[{"user":"168820","topic":"ke
 ## Compare API results to APP results
 curl -s http://localhost:8000/outliers -d '{"dataset":"guardian.2016","param":{"select":[{"dim":"topic","select":"some","head":5},{"dim":"time","select":"all"}],"normalise":["topic","time"],"stat.test":{"type":"poisson","threshold":3}}}'
 
-## Test full pipeline
-curl -H "Content-Type: application/json" -s https://www.fcg-net.org/penelope/data/comment_structure -d '{"collection":"GuardianArticles", "start_date":"2017-01-02T00:00:00.000Z", "end_date":"2017-01-30T00:00:00.000Z", "limit":1}' > guardian.raw
 
-echo '{"data":'$(< guardian.raw)',"param":{"time":{"reference":"comment","resolution":"day"}}}' | curl -s https://penelope.huma-num.fr/tools/format_comments -d @- > guardian.cube
+## ONLINE TESTS
 
-echo '{"data":'$(< guardian.cube)',"param":{"select":[{"dim":"time","select":"all"},{"dim":"topic","select":"some","head":5},{"dim":"user","select":"some","head":30}],"normalise":["topic","time"],"stat.test":{"type":"poisson","threshold":3}}}' | curl -s https://penelope.huma-num.fr/datastore/outliers -d @- > guardian.outliers
+## Check connexion with the Web service
+curl -s https://penelope.huma-num.fr/tools/ping
+
+## Find outliers in a preliminary stored Guardian dataset
+curl -s https://penelope.huma-num.fr/tools/outliers -d '{"dataset": "guardian.2016", "param": {"select": [{"dim": "topic", "select": "some", "head": 5}, {"dim": "week", "select": "all"}], "normalise": ["topic", "week"], "stat.test": {"type": "poisson", "threshold": 1}}}'
+
+## Get Guardian data from www.fcg-net.org
+curl -H "Content-Type: application/json" -s https://www.fcg-net.org/penelope/data/comment_structure -d '{"collection": "GuardianArticles", "start_date": "2017-01-01T00:00:00.000Z", "end_date": "2017-06-01T00:00:00.000Z"}' > guardian.raw
+
+## Reformat Guardian data
+echo '{"data": '$(< guardian.raw)', "param": {"time": {"reference": "comment", "resolution": "day"}}}' | curl -s https://penelope.huma-num.fr/tools/format_comments -d @- > guardian.cube
+
+## Feed reformatted Guardian data to the Web Service
+echo '{"data": '$(< guardian.cube)', "param": {"select": [{"dim": "time", "select": "all"}, {"dim": "topic", "select": "some", "head": 5}, {"dim": "user", "select": "some", "head": 30}], "normalise": ["topic", "time"], "stat.test": {"type": "poisson", "threshold": 1}}}' | curl -s https://penelope.huma-num.fr/tools/outliers -d @- > guardian.outliers
+
 
